87902b675a0ed3d0466aefe23f61a8b7f846e1f8
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2022-12-07 22:02:00 +0200

    hardhat config changes

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,10 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-waffle";
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  paths: {
+    artifacts: "../artifacts",
+  },
 };
 
 export default config;
