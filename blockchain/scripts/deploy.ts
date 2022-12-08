92bfd9a21c9612c2ef64285132d353fead62952c
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2022-12-08 18:22:21 +0200

    re-wrote the deploy script

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,23 +1,15 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+(async () => {
+  const contract = await (await ethers.getContractFactory("TokenName")).deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await contract.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+  console.log(`Contract deployed to ${contract.address}`);
+})().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
